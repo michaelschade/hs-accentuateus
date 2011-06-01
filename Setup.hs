6e656da629d631de26def4855b37bc45cf7a354c
Michael Schade <michael@spearheaddev.com>
2011-05-31 20:52:02 -0500

    Upgrade to version 0.9.2

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,3 @@
-module Main where
+import Distribution.Simple
 
-import System.Process (system)
-
-import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, runTests)
-
-main :: IO ()
-main =
-    defaultMainWithHooks $ simpleUserHooks { runTests = runTests' }
-  where
-    runTests' _ _ _ _ = do
-        system "runhaskell -Wall -i./src src/test.hs"
-        return ()
+main = defaultMain
--- src/test.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import qualified Text.AccentuateUs as AU
-
-main :: IO ()
-main = do
-    print =<< AU.langs (Just "en") 9
-    print =<< AU.accentuate "haw" Nothing "piapa Hawaii"
